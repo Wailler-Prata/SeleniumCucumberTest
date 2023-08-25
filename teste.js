aa2f50cb25f6dc27dfc539e6d1c3e9ceb474873c
Wailler-Prata <wailler.p.b@outlook.com>
2023-08-25 17:33:40 -0300

    teste2

--- teste.js
+++ teste.js
@@ -1,5 +1,22 @@
+const assert = require('assert');
 const { Builder} = require('selenium-webdriver');
 
 const driver = new Builder().forBrowser('MicrosoftEdge').build();
 
-driver.get('https://www.google.com.br/webhp?authuser=1')
+
+
+async function teste (){
+    await driver.get('https://www.google.com.br/webhp?authuser=1')
+    const title = await driver.getTitle()
+    console.log(
+        ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+        title,
+        "Google",
+        assert.strictEqual(title, "Google"),
+        "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+            )
+    
+    await driver.quit()
+}
+
+teste()
