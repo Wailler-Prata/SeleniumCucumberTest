ecd3cf1b5047b6abf62361043ecdce8acb4d9b4f
Wailler-Prata <wailler.p.b@outlook.com>
2023-08-26 15:18:23 -0300

    Ajustes no script ci

--- features/Objects/webPage.js
+++ features/Objects/webPage.js
@@ -1,15 +1,22 @@
 const { Builder } = require('selenium-webdriver');
-const edge = require('selenium-webdriver/edge');
+const firefox = require('selenium-webdriver/firefox');
 
 const edgeOptions = new edge.Options()
   .addArguments('--headless')
   .addArguments('--disable-gpu')
 
+
+  const firefoxOptions = new firefox.Options()
+  .addArguments('--headless')
+  .addArguments('--disable-gpu')
+
+
 module.exports = class WebPage{
     
     constructor(url){
         this.url = url
-        this.driver = new Builder().forBrowser('MicrosoftEdge').setEdgeOptions(edgeOptions).build()
+        //this.driver = new Builder().forBrowser('MicrosoftEdge').setEdgeOptions(edgeOptions).build()
+        this.driver = new Builder().forBrowser('firefox').setFirefoxOptions(firefoxOptions).build()
     }
 
     async openPage(){
